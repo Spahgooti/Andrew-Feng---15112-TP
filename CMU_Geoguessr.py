f6cf52cb87ecd401e5890ee4013563ab42ec8656
Spahgooti <afeng0011@gmail.com>
2025-07-26 09:55:52 -0400

    Dragging Images
    figured out how to drag images (there were a lot of bad typos)

--- CMU_Geoguessr.py
+++ CMU_Geoguessr.py
@@ -1,9 +1,10 @@
 from cmu_graphics import *
+import os
 from PIL import Image
 
 class Location():
-    def __init__(self, name, locX, locY, difficulty, fileName):
-        self.name = name
+    def __init__(self, locX, locY, difficulty, fileName):
+
         self.locX = locX
         self.locY = locY
 
@@ -13,7 +14,9 @@ class Location():
         self.difficulty = difficulty
 
         self.image = Image.open(fileName) 
-    
+
+        # some cool images will get custom names
+        self.name = None
 
     def draw(self, dx, dy, scale, app):
         
@@ -27,6 +30,14 @@ class Location():
         imageCenterX, imageCenterY = app.width/2 + dx, app.height/2 + dy
         drawImage(scaledImage, imageCenterX, imageCenterY + dy, align = 'center')
 
+def importImages(app):
+    listOfImages = []
+    path = './Locations'
+    files = os.listdir(path)
+    
+    for file in files:
+        app.allLocations.append(CMUImage(Image.open(os.path.join(path, file))))
+    return listOfImages
 
 def onAppStart(app):
     # from my phone's camera
@@ -35,8 +46,19 @@ def onAppStart(app):
     app.defaultImageScale = 0.5
 
     # Variables that stay constant over all games
-    app.allLocations = [] # figure out how to add all the locations later
 
+    # for image in Locations:
+    #     #tempFile = image
+    #     # locX = ?? probably some gps data
+    #     # locY = ??
+    #     # difficulty - has to be set manually, maybe itd be easier to keep track of the photos with an id?
+    #     pass
+
+
+    app.allLocations = [] # figure out how to add all the locations later
+    importImages(app)
+
+        
     # Variables that pertain to a specific game
     app.gameDifficulty = None # difficulty of the game
     app.gameLocations = []
@@ -44,8 +66,14 @@ def onAppStart(app):
     app.score = 0
 
     app.imageScale = 1
-    app.imageCX = app.width // 2
-    app.imageCY = app.height // 2
+    # differences between the x and y coordinates of the images center with the center of the canvas
+    app.totalImageDX = 0
+    app.totalImageDY = 0
+    app.currDragImageDX = 0
+    app.currDragImageDY = 0
+    app.isDraggingImage = False
+    app.startDragX = 0
+    app.startDragY = 0
 
     app.mapScale = 1
     app.mapX = None
@@ -54,54 +82,93 @@ def onAppStart(app):
     app.guessX = None
     app.guessY = None
 
-    # Colors
-    app.bgColor1 = 'indigo'
-    app.bgColor2 = 'darkSlateBlue'
-
-
 ################################################################################
 # Starting Screen
 ################################################################################
 
 def starting_redrawAll(app):
-    # l1 = Location("Cathedral View", 20, 20, 3, "Locations/PXL_20250724_165953836.jpg")
-    # l1.draw(0, 0, 0.5, app)
-    backgroundGradient = gradient('indigo', 'darkSlateBlue', start = 'left')
+
+    backgroundGradient = gradient('indigo', 'darkSlateBlue', start = 'bottom')
     normalColor = rgb(151, 232, 81)
     hardColor = rgb(233, 69, 96)
 
     drawRect(0, 0, app.width, app.height, fill=backgroundGradient)
     drawLabel("CMU Geoguessr", app.width // 2, 200, size=96, fill = 'white')
 
-    drawRect(app.width // 2, 600, 200, 100, fill = normalColor, align = 'center')
-    drawRect(app.width // 2, 800, 200, 100, fill = hardColor, align = 'center')
-    drawLabel("Normal Mode", app.width // 2, 600, size = 24, fill = 'white')
-    drawLabel("Hard Mode", app.width // 2, 600, size = 24, fill = 'white')    
+    buttonWidth = 200
+    buttonHeight = 100
+    drawRect(app.width // 2, 550, buttonWidth, buttonHeight, 
+             fill = normalColor, align = 'center')
+    drawRect(app.width // 2, 700, buttonWidth, buttonHeight, 
+             fill = hardColor, align = 'center')
+    
+    drawLabel("Normal Mode", app.width // 2, 550, size = 24, fill = 'white')
+    drawLabel("Hard Mode", app.width // 2, 700, size = 24, fill = 'white') 
+    # drawImage(app.allLocations[0], 0, 0)   
 
+def starting_onMousePress(app, mouseX, mouseY):
+    buttonWidth = 200
+    buttonHeight = 100
+    # user clicked normal mode
+    if (app.width//2 - buttonWidth//2 <= mouseX <= app.width//2 + buttonWidth//2 and
+        550 - buttonHeight//2 <= mouseY <= 550 + buttonHeight//2):
+        app.gameDifficulty = "Normal"
+        # app.gameLocations = getGameLocations(app)
+        setActiveScreen('guessing')
+    
+    # user clicked hard mode
+    if (app.width//2 - buttonWidth//2 <= mouseX <= app.width//2 + buttonWidth//2 and
+        700 - buttonHeight//2 <= mouseY <= 700 + buttonHeight//2):
+        app.gameDifficulty = "Hard"
+        # app.gameLocations = getGameLocations(app)
+        setActiveScreen('guessing')
+
+def getGameLocations(app):
     pass
 
 ################################################################################
 # Guessing Screen
 ################################################################################
 
-def guessing_onScreenActivate(app):
-    # get 5 locations of the right difficulty into a list
-    pass
-
-def guessing_redrawllAll(app):
+def guessing_redrawAll(app):
     # display the first image over the full screen
     # display the map on the bottom right
-    pass
+    imageCenterX = app.width // 2 + app.currDragImageDX + app.totalImageDX
+    imageCenterY = app.height // 2 + app.currDragImageDY + app.totalImageDY
+
+
+    # print(imageCenterX, imageCenterY)
+    drawImage(app.allLocations[0], imageCenterX, imageCenterY, align = "center")
+
+
+def guessing_onMousePress(app, mouseX, mouseY):
+    # if the mouse is on the map, enter a guess on the map
+    # set a variable like "app.guessEntered" to True
+
+    # if the mouse is on the image of the location
+    app.startDragX = mouseX
+    app.startDragY = mouseY
+
+ 
 
 def guessing_onMouseDrag(app, mouseX, mouseY):
     # if the mouse is on the image and not the map, drag the image
     # if the mouse is on the map, drag the map
-    pass
 
-def guessing_onMouseClick(app, mouseX, mouseY):
-    # if the mouse is on the map, enter a guess on the map
-    # set a variable like "app.guessEntered" to True
-    pass
+    app.currDragImageDX = mouseX - app.startDragX
+    app.currDragImageDY = mouseY - app.startDragY
+
+
+
+def guessing_onMouseRelease(app, mouseX, mouseY):
+
+    app.totalImageDX += app.currDragImageDX
+    app.totalImageDY += app.currDragImageDY
+    app.currDragImageDX = 0
+    app.currDragImageDY = 0
+
+
+
 
 def guessing_onKeyPress(app, key):
     # if a guess has been entered, actually enter the guess
@@ -119,8 +186,8 @@ def postGuess_onScreenActivate(app):
 
 
 def main():
-    runAppWithScreens(width = 4032//3, height = 3024//3, 
+    runAppWithScreens(width = 4032//4, height = 3024//4, 
                       initialScreen = 'starting')
-    pass
+
 
 main()
