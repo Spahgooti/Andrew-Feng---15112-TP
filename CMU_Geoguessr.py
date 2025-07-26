d706d8f54d031b9ca1348ca8a7afb720b6fecd40
Spahgooti <afeng0011@gmail.com>
2025-07-26 14:34:46 -0400

    Bounding Image
    added bounds for where the user could move the image

--- CMU_Geoguessr.py
+++ CMU_Geoguessr.py
@@ -13,22 +13,20 @@ class Location():
         # images with difficulty 1-2
         self.difficulty = difficulty
 
-        self.image = Image.open(fileName) 
+        self.image = Image.open(fileName)
+        self.image = CMUImage(self.image)
 
         # some cool images will get custom names
         self.name = None
 
-    def draw(self, dx, dy, scale, app):
+    def draw(self, app, dx, dy, scale):
         
-        imageWidth, imageHeight = self.image.size
-        print(imageWidth, imageHeight)
+        imageWidth, imageHeight = getImageSize(self.image)
         newWidth, newHeight = int(imageWidth * scale), int(imageHeight * scale)
 
-        scaledImage = self.image.resize((newWidth, newHeight))
-        scaledImage = CMUImage(scaledImage)
-
-        imageCenterX, imageCenterY = app.width/2 + dx, app.height/2 + dy
-        drawImage(scaledImage, imageCenterX, imageCenterY + dy, align = 'center')
+        imageCenterX, imageCenterY = app.width//2 + dx, app.height//2 + dy
+        drawImage(self.image, imageCenterX, imageCenterY, align = 'center', 
+                  width = newWidth, height = newHeight)
 
 def importImages(app):
     listOfImages = []
@@ -46,6 +44,8 @@ def onAppStart(app):
     app.defaultImageScale = 0.5
 
     # Variables that stay constant over all games
+    app.defaultImageWidth = 4032
+    app.defaultImageHeight = 3024
 
     # for image in Locations:
     #     #tempFile = image
@@ -65,13 +65,15 @@ def onAppStart(app):
     app.round = 0 # indexes into app.gameLocations, with 5 total rounds (0 - 4)
     app.score = 0
 
-    app.imageScale = 1
-    # differences between the x and y coordinates of the images center with the center of the canvas
+    app.imageScale = 0.4
+    # These variables help store the differences between the x and y coordinates  
+    # of the image's center with the center of the canvas
     app.totalImageDX = 0
     app.totalImageDY = 0
     app.currDragImageDX = 0
     app.currDragImageDY = 0
-    app.isDraggingImage = False
+    app.isDraggingImage = False  # the user could be dragging the map, not the photo
+    
     app.startDragX = 0
     app.startDragY = 0
 
@@ -81,6 +83,8 @@ def onAppStart(app):
 
     app.guessX = None
     app.guessY = None
+
+    app.l1 = Location(0, 0, 0, "Locations/PXL_20250724_165250547.jpg")
 
 ################################################################################
 # Starting Screen
@@ -97,13 +101,13 @@ def starting_redrawAll(app):
 
     buttonWidth = 200
     buttonHeight = 100
-    drawRect(app.width // 2, 550, buttonWidth, buttonHeight, 
+    drawRect(app.width // 2, 525, buttonWidth, buttonHeight, 
              fill = normalColor, align = 'center')
-    drawRect(app.width // 2, 700, buttonWidth, buttonHeight, 
+    drawRect(app.width // 2, 650, buttonWidth, buttonHeight, 
              fill = hardColor, align = 'center')
     
-    drawLabel("Normal Mode", app.width // 2, 550, size = 24, fill = 'white')
-    drawLabel("Hard Mode", app.width // 2, 700, size = 24, fill = 'white') 
+    drawLabel("Normal Mode", app.width // 2, 525, size = 24, fill = 'white')
+    drawLabel("Hard Mode", app.width // 2, 650, size = 24, fill = 'white') 
     # drawImage(app.allLocations[0], 0, 0)   
 
 def starting_onMousePress(app, mouseX, mouseY):
@@ -111,14 +115,14 @@ def starting_onMousePress(app, mouseX, mouseY):
     buttonHeight = 100
     # user clicked normal mode
     if (app.width//2 - buttonWidth//2 <= mouseX <= app.width//2 + buttonWidth//2 and
-        550 - buttonHeight//2 <= mouseY <= 550 + buttonHeight//2):
+        525 - buttonHeight//2 <= mouseY <= 525 + buttonHeight//2):
         app.gameDifficulty = "Normal"
         # app.gameLocations = getGameLocations(app)
         setActiveScreen('guessing')
     
     # user clicked hard mode
     if (app.width//2 - buttonWidth//2 <= mouseX <= app.width//2 + buttonWidth//2 and
-        700 - buttonHeight//2 <= mouseY <= 700 + buttonHeight//2):
+        650 - buttonHeight//2 <= mouseY <= 650 + buttonHeight//2):
         app.gameDifficulty = "Hard"
         # app.gameLocations = getGameLocations(app)
         setActiveScreen('guessing')
@@ -133,13 +137,12 @@ def getGameLocations(app):
 def guessing_redrawAll(app):
     # display the first image over the full screen
     # display the map on the bottom right
-    imageCenterX = app.width // 2 + app.currDragImageDX + app.totalImageDX
-    imageCenterY = app.height // 2 + app.currDragImageDY + app.totalImageDY
+    imageCenterDX = app.currDragImageDX + app.totalImageDX
+    imageCenterDY = app.currDragImageDY + app.totalImageDY
 
 
-    # print(imageCenterX, imageCenterY)
-    drawImage(app.allLocations[0], imageCenterX, imageCenterY, align = "center")
-
+    
+    app.l1.draw(app, imageCenterDX, imageCenterDY, app.imageScale)
 
 def guessing_onMousePress(app, mouseX, mouseY):
     # if the mouse is on the map, enter a guess on the map
@@ -149,7 +152,8 @@ def guessing_onMousePress(app, mouseX, mouseY):
     app.startDragX = mouseX
     app.startDragY = mouseY
 
- 
+    
+
 
 def guessing_onMouseDrag(app, mouseX, mouseY):
     # if the mouse is on the image and not the map, drag the image
@@ -158,10 +162,30 @@ def guessing_onMouseDrag(app, mouseX, mouseY):
     app.currDragImageDX = mouseX - app.startDragX
     app.currDragImageDY = mouseY - app.startDragY
 
+    scaledImageWidth = app.defaultImageWidth * app.imageScale
+    scaledImageHeight = app.defaultImageHeight * app.imageScale
+    imageCenterDX = app.currDragImageDX + app.totalImageDX
+    imageCenterDY = app.currDragImageDY + app.totalImageDY
 
+    # These tests make sure that the image of the location is not dragged off
+    # of the screen.
+    if imageCenterDX >= scaledImageWidth // 2 - app.width // 2:
+        app.totalImageDX = scaledImageWidth // 2 - app.width // 2
+        app.currDragImageDX = 0
+    if imageCenterDX <= -(scaledImageWidth // 2 - app.width // 2):
+        app.totalImageDX = -(scaledImageWidth // 2 - app.width // 2)
+        app.currDragImageDX = 0
+
+    if imageCenterDY >= scaledImageHeight // 2 - app.height // 2:
+        app.totalImageDY = scaledImageHeight // 2 - app.height // 2
+        app.currDragImageDY = 0
+    if imageCenterDY <= -(scaledImageHeight // 2 - app.height // 2):
+        app.totalImageDY = -(scaledImageHeight // 2 - app.height // 2)
+        app.currDragImageDY = 0
 
 def guessing_onMouseRelease(app, mouseX, mouseY):
 
+    # This updates the total distance by which the image has been dragged
     app.totalImageDX += app.currDragImageDX
     app.totalImageDY += app.currDragImageDY
     app.currDragImageDX = 0
@@ -186,7 +210,7 @@ def postGuess_onScreenActivate(app):
 
 
 def main():
-    runAppWithScreens(width = 4032//4, height = 3024//4, 
+    runAppWithScreens(width = 1400, height = 860, 
                       initialScreen = 'starting')
 
 
